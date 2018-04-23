28bd4b8a7f441c8800c9879bad56da5ca1c0ad15
Izabela Kuzniar <izab0583@edu.easj.dk>
2018-04-23 21:24:07 +0200

    ADDED: Tree picture to assets, and MenuTreePage, LogIn as well...

--- StationLogApp/View/MenuTreePage.xaml.cs
+++ StationLogApp/View/MenuTreePage.xaml.cs
@@ -12,25 +12,19 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using StationLogApp.View;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace StationLogApp
+namespace StationLogApp.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MenuTreePage : Page
     {
-        public MainPage()
+        public MenuTreePage()
         {
             this.InitializeComponent();
-        }
-
-        private void LogInButton(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(MenuTreePage));
         }
     }
 }
