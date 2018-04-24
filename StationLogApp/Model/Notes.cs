11e3a37701af875f7a65b5846ae393a1826b7e5b
Milena Ognyanova <mile0416@edu.easj.dk>
2018-04-24 10:50:10 +0200

    Model classes

---
+++ StationLogApp/Model/Notes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StationLogApp.Model
+{
+    public class Notes
+    {
+        #region // Properties
+        public int NotesID { get; set; }
+        public string Note { get; set; }
+        public int StationID { get; set; }
+        public DateTime DueDate { get; set; }
+        public int UserID { get; set; }
+        #endregion
+
+        #region // Constructors 
+        public Notes(int notesID, string note, int stationID, DateTime dueDate, int userID)
+        {
+            NotesID = notesID;
+            Note = note;
+            StationID = stationID;
+            DueDate = dueDate;
+            UserID = userID;
+        }
+
+        public Notes()
+        {
+            
+        }
+        #endregion
+
+        #region // ToString() method
+
+        public override string ToString()
+        {
+            return $"{NotesID}, {Note}, {StationID}, {DueDate}, {UserID}";
+        }
+        #endregion
+    }
+}
---
+++ StationLogApp/Model/Station.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StationLogApp.Model
+{
+    public class Station
+    {
+        #region // Properties
+        public int StationID { get; set; }
+        public string StationName { get; set; }
+        public string StationAddress { get; set; }
+        #endregion
+        #region // Constructors 
+        public Station(int stationID, string stationName, string stationAddress)
+        {
+            StationID = stationID;
+            StationName = stationName;
+            StationAddress = stationAddress;
+        }
+
+        public Station()
+        {
+            
+        }
+        #endregion
+        #region // ToString() method
+
+        public override string ToString()
+        {
+            return $"{StationID}, {StationName}, {StationAddress}";
+        }
+        #endregion
+    }
+}
