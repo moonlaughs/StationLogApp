c3eeaf7c40710335805537d6090cfcc01883e555
Mario Andres Ravina <mari81dh@edu.easj.dk>
2018-04-24 22:29:47 +0200

    Cleaned code of Task History Technician Page Xaml and Task Model Class

--- StationLogApp/Model/Task.cs
+++ StationLogApp/Model/Task.cs
@@ -13,7 +13,7 @@ namespace StationLogApp.Model
 
         private int _taskId;
         private string _taskName;
-        private DateTime _taskSchedule;
+        private string _taskSchedule;
         private string _registration;
         private string _taskType;
         private DateTime _doneDate;
@@ -24,30 +24,18 @@ namespace StationLogApp.Model
         // Properties
 
         public int TaskId { get => _taskId; set => _taskId = value; }
-
         public string TaskName { get => _taskName; set => _taskName = value; }
-
-        public DateTime TaskSchedule { get => _taskSchedule; set => _taskSchedule = value; }
-
+        public string TaskSchedule { get => _taskSchedule; set => _taskSchedule = value; }
         public string Registration { get => _registration; set => _registration = value; }
-
         public string TaskType { get => _taskType; set => _taskType = value; }
-
         public DateTime DoneDate { get => _doneDate; set => _doneDate = value; }
-
         public string Comment { get => _comment; set => _comment = value; }
-
-        public string  DoneVar { get => _doneVar; set => _doneVar = value; }
+        public string DoneVar { get => _doneVar; set => _doneVar = value; }
 
 
         // Constructors
 
-        public Task()
-        {
-            
-        }
-
-        public Task(int taskId, string taskName, DateTime taskSchedule, string registration, string taskType, DateTime doneDate, string comment, string doneVar )
+        public Task(int taskId, string taskName, string taskSchedule, string registration, string taskType, DateTime doneDate, string comment, string doneVar)
         {
             _taskId = taskId;
             _taskName = taskName;
@@ -57,14 +45,19 @@ namespace StationLogApp.Model
             _doneDate = doneDate;
             _comment = comment;
             _doneVar = doneVar;
-            _doneVar = doneVar;
         }
+
+        public Task()
+        {
+            
+        }
+
 
         // Methods
 
         public override string ToString()
         {
-            return $"{_taskId} {_taskName} {_taskSchedule} {_registration} {_taskType} {_doneDate} {_comment} {_doneVar}";
+            return $"{_taskId}, {_taskName}, {_taskSchedule}, {_registration}, {_taskType}, {_doneDate}, {_comment}, {_doneVar}";
         }
     }
 }
