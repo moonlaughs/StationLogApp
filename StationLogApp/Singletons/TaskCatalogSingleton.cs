de4a8cebea5a8ea3533035f24b3b71c7282b1cfc
Izabela Kuzniar <izab0583@edu.easj.dk>
2018-04-25 12:17:11 +0200

    no message

--- StationLogApp/Singletons/TaskCatalogSingleton.cs
+++ StationLogApp/Singletons/TaskCatalogSingleton.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using StationLogApp.Factories;
-using StationLogApp.Model;
-using StationLogApp.Persistancy;
 
-namespace StationLogApp.Common
+namespace StationLogApp.Singletons
 {
     public class TaskCatalogSingleton
     {
