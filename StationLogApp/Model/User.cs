1e53a23689f768d58817bea335b493c936449d14
Diiiya <33103806+Diiiya@users.noreply.github.com>
2018-04-25 09:08:04 +0300

    Added User class in Model

---
+++ StationLogApp/Model/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StationLogApp.Model
+{
+    public class User
+    {
+        #region Properties
+        public string UserID { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string UserPassword { get; set; }
+        public string UserType { get; set; }
+        #endregion
+
+        #region Constructors
+        public User(string userID, string firstname, string surname, string username, string userPassword, string userType)
+        {
+            UserID = userID;
+            Firstname = firstname;
+            Surname = surname;
+            Username = username;
+            UserPassword = userPassword;
+            UserType = userType;
+        }
+
+        public User()
+        {
+            
+        }
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return $"{UserID}, {Firstname}, {Surname}, {Username}, {UserPassword}, {UserType}";
+        }
+
+        #endregion
+    }
+}
