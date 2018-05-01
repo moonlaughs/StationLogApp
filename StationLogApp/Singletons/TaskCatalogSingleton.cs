01b342121c1c287b8926953483a0511158a40092
Mario Andres Ravina <mari81dh@edu.easj.dk>
2018-05-01 15:58:10 +0200

    local commit

--- StationLogApp/Singletons/TaskCatalogSingleton.cs
+++ StationLogApp/Singletons/TaskCatalogSingleton.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using StationLogApp.Factories;
 using StationLogApp.Interfaces;
+using StationLogApp.Model;
 using StationLogApp.Persistancy;
 
 namespace StationLogApp.Singletons
@@ -22,12 +24,29 @@ namespace StationLogApp.Singletons
 
         private TaskCatalogSingleton()
         {
-            TaskCatalog = new ObservableCollection<ITaskFactory>();
-            ILoad<ITaskFactory> retrievedCatalog = new LoadTask<ITaskFactory>();
+            
+            TaskCatalog = new ObservableCollection<ITaskFactory>(); 
+            
+            //ILoad<User> loaded = new LoadM<User>();
+            //Task<ObservableCollection<User>> sth = loaded.Load();
+            //await sth;
+            //ObservableCollection<User> col = sth.Result;
+
+           
+            
+            
             //TaskCatalog = new ObservableCollection<ITaskFactory>(retrievedCatalog.Load()); 
             TaskCatalog = retrievedCatalog.Load();
         }
         
+        public static async Task<ObservableCollection<TaskClass>> LoadCatalog()
+        {
+            ILoad<TaskClass> retrievedCatalog = new LoadTask<TaskClass>();
+            Task<ObservableCollection<TaskClass>> sth = retrievedCatalog.Load();
+            await sth;
+            ObservableCollection<TaskClass> col = sth.Result;
+            
+        }
 
         // Singleton Method
 
