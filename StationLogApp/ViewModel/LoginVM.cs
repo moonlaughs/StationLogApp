b221510eddc5029e7ab8d03b655c9f4032bf79c0
Izabela Kuzniar <izab0583@edu.easj.dk>
2018-05-02 10:49:03 +0200

    ...

--- StationLogApp/ViewModel/LoginVM.cs
+++ StationLogApp/ViewModel/LoginVM.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using Windows.UI.Popups;
 using StationLogApp.Common;
 using StationLogApp.Interfaces;
-using StationLogApp.Methods;
 using StationLogApp.Model;
 using StationLogApp.Persistancy;
 using StationLogApp.Singletons;
@@ -18,22 +17,52 @@ namespace StationLogApp.ViewModel
     public class LoginVM : NotifyPropertyChangedClass
     {
         private IUserFactory _currentUser = new User();
-        public RelayCommandClass CheckCommand { get; set; }
         private readonly FrameNavigateClass _frame;
         private readonly UserSingleton _userSingleton;
-        private bool LoginStatus { get; set; }
-        
 
+        private bool LoginStatus { get; set; }
+        public RelayCommandClass CheckCommand { get; set; }
+        public IUserFactory CurrentUser
+        {
+            get => _currentUser;
+            set
+            {
+                _currentUser = value;
+                OnPropertyChanged(nameof(CurrentUser));
+            }
+        }
         public LoginVM()
         {
-
             _frame = new FrameNavigateClass();
             _userSingleton = UserSingleton.GetInstance();
-            LoginCheck loginCheck = new LoginCheck();
-            CheckCommand = new RelayCommandClass(loginCheck.Check);
+            CheckCommand = new RelayCommandClass(Check);
         }
 
-
+        public async void Check()
+        {
+            LoginStatus = false;
+            ILoad<IUserFactory> loaded = new LoadM<IUserFactory>();
+            Task<ObservableCollection<User>> sth = loaded.Load("UserTables");
+            await sth;
+            ObservableCollection<User> col = sth.Result;
+            if (col != null)
+            {
+                foreach (User user in col)
+                {
+                    if ((user.Username == CurrentUser.Username) && (user.UserPassword == CurrentUser.UserPassword))
+                    {
+                        _userSingleton.SetPerson(user);
+                        LoginStatus = true;
+                        _frame.ActivateFrameNavigation(typeof(MenuTreePage), user);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                MessageDialog msg = new MessageDialog("No user found with that username and password!");
+                await msg.ShowAsync();
+            }
+        }
     }
-
 }
