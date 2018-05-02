db807ff1bfced47e156044df9fd40d4053276b32
Diiiya <33103806+Diiiya@users.noreply.github.com>
2018-05-02 11:06:53 +0300

    Just deleted another unused code

--- StationLogApp/Persistancy/LoadM.cs
+++ StationLogApp/Persistancy/LoadM.cs
@@ -14,66 +14,6 @@ using StationLogApp.Model;
 
 namespace StationLogApp.Persistancy
 {
-    //public class LoadM<T> : ILoad<T> where T : class
-    //{
-    //    #region instancefields
-
-    //    private const string ServerUrl = "http://stationlogwebservice20180424112310.azurewebsites.net";
-
-    //    private string _serverURL;
-    //    private string _apiPrefix = "api";
-    //    private string _apiID;
-    //    private HttpClientHandler _httpClientHandler;
-    //    private HttpClient _httpClient;
-
-    //    #endregion
-
-    //    public async Task<ObservableCollection<User>> Load(string _apiID)
-    //    {
-    //        _httpClientHandler = new HttpClientHandler() {UseDefaultCredentials = true};
-    //        using (_httpClient = new HttpClient(_httpClientHandler))
-    //        {
-    //            _httpClient.BaseAddress = new Uri(ServerUrl);
-    //            _httpClient.DefaultRequestHeaders.Clear();
-    //            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-    //            try
-    //            {
-    //                Task<HttpResponseMessage> task5 = _httpClient.GetAsync($"{ServerUrl}/{_apiPrefix}/{_apiID}");
-    //                if (task5 != null)
-    //                {
-    //                    if (task5.Result.IsSuccessStatusCode)
-    //                    {
-    //                        var task51 = await task5.Result.Content.ReadAsStringAsync();
-    //                       // ObservableCollection<T> listt = JsonConvert.DeserializeObject<ObservableCollection<T>>(task51);
-    //                        ObservableCollection<User> listt = DeserializeUser(task51);
-    //                        return listt;
-    //                    }
-    //                }
-    //            }
-    //            catch (Exception ex)
-    //            {
-    //                await new MessageDialog(ex.Message).ShowAsync();
-    //            }
-    //            return null;
-    //        }
-    //    }
-
-    //    private static ObservableCollection<User> DeserializeUser(string json)
-    //    {
-    //        //GenericJsonConverter<T> userConverter = new GenericJsonConverter<T>();
-    //        //var settings = new JsonSerializerSettings();
-    //        //settings.Converters.Add(new GenericJsonConverter<T>());
-    //        //ObservableCollection<T> userConverted = JsonConvert.DeserializeObject<ObservableCollection<T>>(json, settings);
-    //        //return userConverted;
-    //        UserConverter userConverter = new UserConverter();
-    //        var settings = new JsonSerializerSettings();
-    //        settings.Converters.Add(new UserConverter());
-    //        ObservableCollection<User> userConverted = JsonConvert.DeserializeObject<ObservableCollection<User>>(json, settings);
-    //        return userConverted;
-
-    //    }
-    //}
-
     public class LoadM<T> : ILoad<T> where T : class
     {
         #region instancefields
