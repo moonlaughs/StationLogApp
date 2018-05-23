bce872b5d072859851ec87660e9820be405efaaa
Diiiya <33103806+Diiiya@users.noreply.github.com>
2018-05-23 12:58:32 +0300

    UnitTest DeeleteTaskHandler

--- UnitTestStationLogApp/UnitTest.cs
+++ UnitTestStationLogApp/UnitTest.cs
@@ -1,6 +1,9 @@
 ﻿
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StationLogApp.Common;
+using StationLogApp.Handlers;
+using StationLogApp.ViewModel;
 
 namespace UnitTestStationLogApp
 {
@@ -16,9 +19,27 @@ namespace UnitTestStationLogApp
     [TestClass]
     public class UnitTestDeleteTaskHandler
     {
+        public DeleteTaskHandler DeleteTaskHandler { get; set; }
+        public TaskVm TaskVm { get; set; }
+        public Collections Collections { get; set; }
+
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            TaskVm = new TaskVm();
+            DeleteTaskHandler = new DeleteTaskHandler(TaskVm);
+            Collections = new Collections();
+        }
+
         [TestMethod]
         public void TestDeleteTask()
         {
+            TaskVm.SelectedItem.TaskId = 221;
+            DeleteTaskHandler.DeleteTask(TaskVm.SelectedItem.TaskId);
+            int myValue = 80;
+            int realValue = Collections.LoadToDo().Count;
+            Assert.AreNotEqual(myValue, realValue);
+
         }
     }
 }
