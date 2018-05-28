717c4f6560adf4d7b614e0d7a4844941ec55bf71
Diiiya <33103806+Diiiya@users.noreply.github.com>
2018-05-28 12:10:48 +0300

    MainPage GUI changes

--- UnitTestStationLogApp/UnitTest.cs
+++ UnitTestStationLogApp/UnitTest.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StationLogApp.Common;
+using StationLogApp.Converters;
 using StationLogApp.Handlers;
 using StationLogApp.ViewModel;
 
@@ -35,11 +36,37 @@ namespace UnitTestStationLogApp
         public void TestDeleteTask()
         {
             TaskVm.SelectedItem.TaskId = 221;
+            var b = TaskVm.SelectedItem != null;
             DeleteTaskHandler.DeleteTask(TaskVm.SelectedItem.TaskId);
             int myValue = 80;
             int realValue = Collections.LoadToDo().Count;
             Assert.AreNotEqual(myValue, realValue);
 
         }
+    }
+
+    [TestClass]
+    public class UnitTestCreateTaskHandler
+    {
+        public CreateTaskHandler CreateTaskHandler { get; set; }
+        public CreateTaskVm CreateTaskVm { get; set; }
+        public DateConverter DateConverter { get; set; }
+        public Collections Collections { get; set; }
+
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            CreateTaskVm = new CreateTaskVm();
+            CreateTaskHandler = new CreateTaskHandler(CreateTaskVm);
+            DateConverter = new DateConverter();
+            Collections = new Collections();
+        }
+
+        [TestMethod]
+        public void TestCreateTask()
+        {
+            //CreateTaskVm.NewItem.TaskId = 
+        }
+
     }
 }
